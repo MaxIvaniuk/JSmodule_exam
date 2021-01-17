ca99d309e523070c2bf40d36627555049251e731
MaxIvaniuk <meteorit614@gmail.com>
2021-01-18 00:36:23 +0200

    add more styles

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -1,13 +1,13 @@
 "use strict";
 
 $(document).ready(function () {
-  $('.slider').slick({
-    arrows: true,
+  $('.banner__slider').slick({
+    arrows: false,
     dots: true,
     slidesToShow: 1,
-    autoplay: false,
-    speed: 1000,
-    autoplaySpeed: 800,
+    autoplay: true,
+    speed: 1800,
+    autoplaySpeed: 2000,
     responsive: [{
       breakpoint: 768,
       settings: {
@@ -21,3 +21,21 @@ $(document).ready(function () {
     }]
   });
 });
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: -34.397,
+      lng: 150.644
+    },
+    zoom: 10
+  });
+  var marker = new google.maps.Marker({
+    position: {
+      lat: -34.397,
+      lng: 150.644
+    },
+    map: map
+  });
+}
--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-	$('.slider').slick({
-		arrows:true,
+	$('.banner__slider').slick({
+		arrows:false,
 		dots:true,
 		slidesToShow:1,
-		autoplay:false,
-		speed:1000,
-		autoplaySpeed:800,
+		autoplay:true,
+		speed:1800,
+		autoplaySpeed:2000,
 		responsive:[
 			{
 				breakpoint: 768,
@@ -23,3 +23,15 @@ $(document).ready(function(){
 	});
 });
 
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 10,
+  });
+  const marker = new google.maps.Marker({
+    position: { lat: -34.397, lng: 150.644 },
+    map: map,
+  });
+}
