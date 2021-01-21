6708faadf2f94c348ddffd20b8ba929563ff9b61
MaxIvaniuk <meteorit614@gmail.com>
2021-01-21 22:47:07 +0200

    add preloader

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -59,4 +59,25 @@ function validate(form_id, email) {
     alert('Please enter correct email');
     return false;
   }
+} // Preloader
+
+
+var hellopreloader = document.getElementById("hellopreloader_preload");
+
+function fadeOutnojquery(el) {
+  el.style.opacity = 1;
+  var interhellopreloader = setInterval(function () {
+    el.style.opacity = el.style.opacity - 0.05;
+
+    if (el.style.opacity <= 0.05) {
+      clearInterval(interhellopreloader);
+      hellopreloader.style.display = "none";
+    }
+  }, 16);
 }
+
+window.onload = function () {
+  setTimeout(function () {
+    fadeOutnojquery(hellopreloader);
+  }, 500);
+};
--- js/script.js
+++ js/script.js
@@ -60,3 +60,20 @@ function validate(form_id, email) {
 	   return false;
 	}
  }
+
+// Preloader
+let hellopreloader = document.getElementById("hellopreloader_preload");
+function fadeOutnojquery(el){
+	el.style.opacity = 1;
+	let interhellopreloader = setInterval(function(){
+		el.style.opacity = el.style.opacity - 0.05;
+		if (el.style.opacity <=0.05){ 
+			clearInterval(interhellopreloader);
+			hellopreloader.style.display = "none";
+		}
+	},16);
+}
+		window.onload = function(){
+			setTimeout(function(){fadeOutnojquery(hellopreloader);
+		},500);
+	};
